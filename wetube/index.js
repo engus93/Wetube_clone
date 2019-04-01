b7401e842999f39577d115ccd572ce20ad88e43f
Dudu <engus983@gmail.com>
2019-04-01 17:09:40 +0900

    ADD: #2.5 ES6 on NodeJS using Babel
    Babel setup

--- wetube/index.js
+++ wetube/index.js
@@ -1,23 +1,16 @@
-const express = require('express');
+import express from "express";
 const app = express();
 
 const PORT = 3000;
 
-const handleListening = () => {
-    console.log(`Listening on: http://localhost:${PORT}`)
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-const handleHome = (req, res) => {
-    console.log(req);
-    res.send("Hello from home")
-}
+const handleHome = (req, res) => res.send("Hello from home!!!");
 
-const handleProfile = (req, res) => {
-    res.send("You are on my profile")
-}
+const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.get("/",handleHome)
 
 app.get("/profile",handleProfile)
 
-app.listen(PORT, handleListening);
+app.listen(PORT, handleListening)
