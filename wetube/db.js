8c7f4a336239b3b1d12b953b217da0a1c5531295
Dudu <engus983@gmail.com>
2019-04-02 19:54:47 +0900

    ADD: #2.22 Home Controller part Two
    mixin으로 반복되는 element 생성

--- wetube/db.js
+++ wetube/db.js
@@ -4,7 +4,7 @@ export const videos = [
         title: "Video awesome",
         description: "This is something I Love1",
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Dudu",
@@ -16,7 +16,7 @@ export const videos = [
         title: "Video super",
         description: "This is something I Love2",
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Dudu",
@@ -28,7 +28,7 @@ export const videos = [
         title: "Video nice",
         description: "This is something I Love3",
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Dudu",
@@ -40,7 +40,7 @@ export const videos = [
         title: "Video perfect",
         description: "This is something I Love4",
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Dudu",
