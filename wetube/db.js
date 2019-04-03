9b12e3cbaf8e27f7d21473b1ce77816d12203f95
Dudu <engus983@gmail.com>
2019-04-03 15:14:28 +0900

    ADD:  #3.3 Video Model
    MongoDB AWS로 옮김 and create schema and model in Video.js

--- wetube/db.js
+++ wetube/db.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();    //dotenv.config()를 하면 process.env.~로 파일을 다 불러옴
 
-
 mongoose.connect(
     // MongoDB URL을 dotenv를 통해 연결
     process.env.MONGO_URL,
--- wetube/init.js
+++ wetube/init.js
@@ -1,6 +1,9 @@
 import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+
 dotenv.config();    //dotenv.config()를 하면 process.env.~로 파일을 다 불러옴
+import "./models/Video"
 
 // PORT를 dotenv를 통해 연결
 const PORT = process.env.PORT || 3000;
