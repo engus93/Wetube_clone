5f00826a48e0c66bc94eaa50dac60b9a05eb726f
Dudu <engus983@gmail.com>
2019-04-04 15:00:28 +0900

    #3.12 Searching Videos
    search video completed regex 적용 완료

--- wetube/controllers/videoController.js
+++ wetube/controllers/videoController.js
@@ -21,7 +21,15 @@ export const search = async (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy });
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
 // Upload
