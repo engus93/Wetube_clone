b5157244cdb443a71cdfe6f9af879dcecec1ae9d
Dudu <engus983@gmail.com>
2019-04-04 20:05:56 +0900

    ADD: #4.3 ES6 with Webpack
    webpack end

--- wetube/webpack.config.js
+++ wetube/webpack.config.js
@@ -3,15 +3,22 @@ const autoprefixer = require("autoprefixer");
 const ExtractCSS = require("extract-text-webpack-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
-
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
       {
         test: /\.(scss)$/,
         use: ExtractCSS.extract([
@@ -21,7 +28,7 @@ const config = {
           {
             loader: "postcss-loader",
             options: {
-              plugin() {
+              plugins() {
                 return [autoprefixer({ browsers: "cover 99.5%" })];
               }
             }
@@ -35,9 +42,9 @@ const config = {
   },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[js]"
+    filename: "[name].js"
   },
-  plugins: [new ExtractCSS("style.css")]
+  plugins: [new ExtractCSS("styles.css")]
 };
 
 module.exports = config;
