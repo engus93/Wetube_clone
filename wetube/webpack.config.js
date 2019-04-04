5f426dc55e5974c5fc59a1bc3c9b800d73301fb3
Dudu <engus983@gmail.com>
2019-04-04 18:50:46 +0900

    ADD: #4.2 Styles with Webpack part Two
    webpack sass -> css 성공

--- wetube/webpack.config.js
+++ wetube/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const autoprefixer = require("autoprefixer");
 const ExtractCSS = require("extract-text-webpack-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
@@ -18,7 +19,12 @@ const config = {
             loader: "css-loader"
           },
           {
-            loader: "postcss-loader"
+            loader: "postcss-loader",
+            options: {
+              plugin() {
+                return [autoprefixer({ browsers: "cover 99.5%" })];
+              }
+            }
           },
           {
             loader: "sass-loader"
@@ -29,8 +35,9 @@ const config = {
   },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[format]"
-  }
+    filename: "[name].[js]"
+  },
+  plugins: [new ExtractCSS("style.css")]
 };
 
 module.exports = config;
