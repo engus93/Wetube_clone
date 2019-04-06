eec95fef182f9faed1140078408f57eb38702c05
Dudu <engus983@gmail.com>
2019-04-06 23:47:31 +0900

    ADD: #6.6 Github Log In part One
    github authentication working

--- wetube/passport.js
+++ wetube/passport.js
@@ -1,7 +1,17 @@
 import passport from "passport";
+import GihubStrategy from "passport-github";
 import User from "./models/User";
+import { githubLoginCallback } from "./controllers/userController";
 
 passport.use(User.createStrategy());
+passport.use(
+  new GihubStrategy({
+    clientID: process.env.GH_ID,
+    clientSecret: process.env.GH_SECRET,
+    callbackURL: "http://localhost:3000/auth/github/callback"
+  }),
+  githubLoginCallback
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
