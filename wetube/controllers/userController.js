5eb21f98714bcd7d7bec6c4bb0285e9f798b0eab
Dudu <engus983@gmail.com>
2019-04-06 17:52:06 +0900

    ADD: #6.3 Loggin the User In
    login seccess and authentication completed

--- wetube/controllers/userController.js
+++ wetube/controllers/userController.js
@@ -1,4 +1,5 @@
 // User Controller
+import passport from "passport";
 import routes from "../routes";
 import User from "../models/User";
 
@@ -6,7 +7,7 @@ export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = async (req, res) => {
+export const postJoin = async (req, res, next) => {
   const {
     body: { name, email, password, password2 }
   } = req;
@@ -20,19 +21,22 @@ export const postJoin = async (req, res) => {
         email
       });
       await User.register(user, password);
+      next();
     } catch (error) {
       console.log(error);
+      res.redirect(routes.home);
     }
-    // To Do: Log user in``
-    res.redirect(routes.home);
+    // To Do: Log user in
   }
 };
 
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Log In" });
-export const postLogin = (req, res) => {
-  res.redirect(routes.home);
-};
+
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home
+});
 
 export const logout = (req, res) => {
   // To Do: Process Log Out
@@ -41,7 +45,9 @@ export const logout = (req, res) => {
 
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
+
 export const editProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
+
 export const changePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "Change Password" });
