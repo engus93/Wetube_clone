5a8617cdcb8ab6bb227a20e1179250c705f454ef
Dudu <engus983@gmail.com>
2019-04-09 21:14:02 +0900

    FEAT: #9.2 Recording video part Two
    Added function that is a recording and the record file down

--- wetube/assets/js/videoRecorder.js
+++ wetube/assets/js/videoRecorder.js
@@ -3,16 +3,29 @@ const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
 let streamObject;
+let videoRecorder;
 
 const handleVideoData = event => {
-  console.log(event);
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start recording";
 };
 
 const startRecording = () => {
-  const videoRecorder = new MediaRecorder(streamObject);
-  videoRecorder.start();
-  console.log(videoRecorder);
-  videoRecorder.addEventListener("detaavailable", handleVideoData);
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start(1000);
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
 };
 
 const getVideo = async () => {
@@ -24,7 +37,7 @@ const getVideo = async () => {
     videoPreview.srcObject = stream;
     videoPreview.muted = true;
     videoPreview.play();
-    recordBtn.innerHTML = "Stop Recording";
+    recordBtn.innerHTML = "Stop recording";
     streamObject = stream;
     startRecording();
   } catch (error) {
