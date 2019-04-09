fc2a8cbb4db2f2dd3c363a19b15f9101c3a67756
Dudu <engus983@gmail.com>
2019-04-09 20:35:13 +0900

    FEAT: #9.1 Recording Video part One
    Added function that is a recording

--- wetube/assets/js/videoRecorder.js
+++ wetube/assets/js/videoRecorder.js
@@ -2,7 +2,20 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-const startRecording = async () => {
+let streamObject;
+
+const handleVideoData = event => {
+  console.log(event);
+};
+
+const startRecording = () => {
+  const videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  console.log(videoRecorder);
+  videoRecorder.addEventListener("detaavailable", handleVideoData);
+};
+
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
@@ -11,17 +24,21 @@ const startRecording = async () => {
     videoPreview.srcObject = stream;
     videoPreview.muted = true;
     videoPreview.play();
+    recordBtn.innerHTML = "Stop Recording";
+    streamObject = stream;
+    startRecording();
   } catch (error) {
     recordBtn.innerHTML = "☹ Can't record";
-    recordBtn.removeEventListener("click", startRecording);
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
 function init() {
-  recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
 }
 
-// main.js에 선언하므로 어느 페이지에서든지 실행이 된다 그를 방지하기위해 recorderContainer가 있는지를 확인한다.
+// main.js에 선언하므로 어느 페이지에서든지 실행이 된다 그를 방지하기위해 recorderContainer가 있는 페이지를 찾기 위해 확인한다.
 if (recorderContainer) {
   init();
 }
