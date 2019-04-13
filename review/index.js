3f84fc26e08d1a00f986fd24323ffeeee81f8737
Dudu <engus983@gmail.com>
2019-04-13 13:10:59 +0900

    FEAT: #2.4 Handling Routes with Express
    Added routing handler function => /home, /profile

--- review/index.js
+++ review/index.js
@@ -3,6 +3,18 @@ const app = express();
 
 const PORT = 3000;
 
+function handleHome(req, res) {
+  res.send("Hello from home");
+}
+
+function handleProfile(req, res) {
+  res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
