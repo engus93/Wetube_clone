1b5928f7647848651de823bdb63fb65fda67a97a
Dudu <engus983@gmail.com>
2019-04-16 19:24:30 +0900

    FEAT: #6.6 Github Log In part One
    Added github login setting ing ing ing

--- review/controllers/userController.js
+++ review/controllers/userController.js
@@ -39,9 +39,13 @@ export const postLogin = passport.authenticate("local", {
   successRedirect: routes.home
 });
 
+export const githubLoginCallback = (accessToken, refreshToken, profile, cb) => {
+  console.log(accessToken, refreshToken, profile, cb);
+};
+
 // Log Out
 export const logout = (req, res) => {
-  // To Do: Process Log Out
+  req.logout();
   res.redirect(routes.home);
 };
 
