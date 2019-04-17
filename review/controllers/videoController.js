1e5f98c49ed6c557b936655266c69387b49fc964
Dudu <engus983@gmail.com>
2019-04-17 13:58:57 +0900

    FEAT: #7.3 Adding Creator to Video
    Added video-detail author branch processing

--- review/controllers/videoController.js
+++ review/controllers/videoController.js
@@ -43,8 +43,11 @@ export const postUpload = async (req, res) => {
     const newVideo = await Video.create({
       fileUrl: path,
       title,
-      description
+      description,
+      creator: req.user.id
     });
+    req.user.videos.push(newVideo.id);
+    req.user.save();
     res.redirect(routes.videoDetail(newVideo.id));
   } catch (error) {
     console.log(error);
@@ -58,7 +61,7 @@ export const videoDetail = async (req, res) => {
     params: { id }
   } = req;
   try {
-    const video = await Video.findById(id);
+    const video = await Video.findById(id).populate("creator");
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     console.log(error);
