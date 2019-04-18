71ae46d1823cd6b830db4aa4f8fc3fcc21108e32
Dudu <engus983@gmail.com>
2019-04-18 16:52:34 +0900

    FEAT: #10.4 API Adding a Comment part Three
    Completed real-time comment-write add

--- review/assets/js/addComment.js
+++ review/assets/js/addComment.js
@@ -1,11 +1,24 @@
-// Add Comment
-
 import axios from "axios";
 
 const addCommentForm = document.getElementById("jsAddComment");
+const commentList = document.getElementById("jsCommentList");
+const commentNumber = document.getElementById("jsCommentNumber");
+
+const increaseNumber = () => {
+  commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1;
+};
+
+const addComment = comment => {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerHTML = comment;
+  li.appendChild(span);
+  commentList.prepend(li);
+  increaseNumber();
+};
 
 const sendComment = async comment => {
-  const videoId = window.location.href.split("videos/")[1];
+  const videoId = window.location.href.split("/videos/")[1];
   const response = await axios({
     url: `/api/${videoId}/comment`,
     method: "POST",
@@ -13,7 +26,9 @@ const sendComment = async comment => {
       comment
     }
   });
-  console.log(response);
+  if (response.status === 200) {
+    addComment(comment);
+  }
 };
 
 const handleSubmit = event => {
@@ -24,9 +39,9 @@ const handleSubmit = event => {
   commentInput.value = "";
 };
 
-const init = () => {
+function init() {
   addCommentForm.addEventListener("submit", handleSubmit);
-};
+}
 
 if (addCommentForm) {
   init();
